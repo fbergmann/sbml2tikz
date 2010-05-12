74898a77cf0d8a1c450a0d6a45b2332e8d6b421f
s.yuan.shen <s.yuan.shen@fcf08512-0e57-19ac-9eeb-097739688a13>
2010-05-12 00:30:09 +0000

    Added GUI project; set the solution as SBML2TikZ, the library as libSBML2TikZ and the GUI as SBML2TikZGUI.

--- trunk/SBML2TikZGUI/Properties/AssemblyInfo.cs
+++ trunk/SBML2TikZGUI/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RenderExtension2TikZ")]
+[assembly: AssemblyTitle("RenderExt2TikZ")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RenderExtension2TikZ")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCompany("University of Washington")]
+[assembly: AssemblyProduct("RenderExt2TikZ")]
+[assembly: AssemblyCopyright("Copyright © University of Washington 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9ea18a80-0960-481a-8278-22f44dc4d990")]
+[assembly: Guid("66429d59-17a9-4df4-8a4c-090785ca0a10")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ trunk/SBML2TikZGUI/RunGUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace SBML2TikZ_GUI
+{
+    class RunGUI
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+         
+        }
+    }
+}
